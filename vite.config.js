98d5b8e2ab4331996797aa4a4dff6d1e4814d980
anchalraj31082004 <anchalraj20045@gmail.com>
2024-12-29 11:43:06 +0530

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/anchaldevbytes.github.io/',
   build: {
     outDir: 'dist',
     rollupOptions: {
