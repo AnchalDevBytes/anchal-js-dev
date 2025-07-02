76a9ba267f7348c38197b80428a787fb0a720b88
anchalraj31082004 <anchalraj20045@gmail.com>
2025-07-02 13:48:38 +0530

    updated main again

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/anchal-js-dev',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/index.html', // Ensure this points to your HTML file
-    },
-  },
 });
